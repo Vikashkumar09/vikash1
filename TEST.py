eaa51f9b0b3a8bca44d7245d105d3ffbce6f3426
Vikashkumar09 <95523688+Vikashkumar09@users.noreply.github.com>
2023-06-05 23:12:16 +0530

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Move Zeroes
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+a. 1 <= nums.length <= 10^4
+b. -2^31 <= nums[i] <= 2^31 - 1
+
+Note: Create a GitHub file for the solution and add the file link the the answer section below.
+
+
+# In[ ]:
+
+
+
+def moveZeroes(self, nums):
+    zero = 0  # records the position of "0"
+    for i in xrange(len(nums)):
+        if nums[i] != 0:
+            nums[i], nums[zero] = nums[zero], nums[i]
+            zero += 1
+
